587a8101a98834740bda99d5d21f18077ccb86f7
mrarronz <zhuhao1340@gmail.com>
2018-05-03 10:09:40 +0800

    添加Android端“原生->RN -> 原生”的跳转示例

---
+++ Chapter10-RNInteractionWithNative/NativeJumpToRN/android/app/src/main/java/com/nativejumptorn/LaunchActivity.java
@@ -0,0 +1,27 @@
+package com.nativejumptorn;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Chapter10-RNInteractionWithNative/NativeJumpToRN/android/app/src/main/java/com/nativejumptorn/OpenNativeModule.java
@@ -0,0 +1,34 @@
+package com.nativejumptorn;
+
+import android.content.Intent;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+/**
+ * Created by arronzhu on 2018/5/3.
+ */
+
+public class OpenNativeModule extends ReactContextBaseJavaModule {
+    private ReactContext mReactContext;
+
+    public OpenNativeModule(ReactApplicationContext context) {
+        super(context);
+        this.mReactContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "OpenNativeModule";
+    }
+
+    @ReactMethod
+    public void openNativeVC() {
+        Intent intent = new Intent();
+        intent.setClass(mReactContext, TestActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mReactContext.startActivity(intent);
+    }
+}
