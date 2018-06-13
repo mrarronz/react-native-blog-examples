8960cebb518f17f6df686407375b485e8bae052d
mrarronz <zhuhao1340@gmail.com>
2018-06-13 15:56:09 +0800

    Add comment for iOS project

--- Chapter10-RNInteractionWithNative/DataTransfer/ios/DataTransfer/DataTransferModule.m
+++ Chapter10-RNInteractionWithNative/DataTransfer/ios/DataTransfer/DataTransferModule.m
@@ -42,34 +42,40 @@ RCT_EXPORT_MODULE();
   return YES;
 }
 
+/// RN向原生传递字符串
 RCT_EXPORT_METHOD(getStringFromReactNative:(NSString *)s) {
   NSString *msg = [NSString stringWithFormat:@"RN传递过来的字符串：%@", s];
   [self showAlert:msg];
 }
 
+/// RN向原生端传递整数
 RCT_EXPORT_METHOD(getIntFromReactNative:(NSInteger)i) {
   NSString *msg = [NSString stringWithFormat:@"RN传递过来的整数：%zd", i];
   [self showAlert:msg];
 }
 
+/// RN向原生传递字典
 RCT_EXPORT_METHOD(getDictionaryFromRN:(NSDictionary *)dict) {
   NSLog(@"RN传递过来的字典：%@", dict);
   NSString *name = [dict objectForKey:@"title"];
   [self showAlert:name];
 }
 
+/// RN向原生传递数组
 RCT_EXPORT_METHOD(getArrayFromRN:(NSArray *)array) {
   NSLog(@"RN传递过来的数组：%@", array);
   NSString *joinString = [array componentsJoinedByString:@", "];
   [self showAlert:joinString];
 }
 
+/// 回传字符串到RN端
 RCT_EXPORT_METHOD(passStringBackToRN:(RCTResponseSenderBlock)block) {
   if (block) {
     block(@[@"This is a string from Native"]);
   }
 }
 
+/// 回传字典到RN端
 RCT_EXPORT_METHOD(passDictionaryBackToRN:(RCTResponseSenderBlock)block) {
   if (block) {
     NSDictionary *dict = @{
@@ -82,6 +88,7 @@ RCT_EXPORT_METHOD(passDictionaryBackToRN:(RCTResponseSenderBlock)block) {
   }
 }
 
+/// 回传数组到RN端
 RCT_EXPORT_METHOD(passArrayBackToRN:(RCTResponseSenderBlock)block) {
   if (block) {
     NSArray *items = @[@"React Native", @"Android", @"iOS"];
@@ -89,6 +96,7 @@ RCT_EXPORT_METHOD(passArrayBackToRN:(RCTResponseSenderBlock)block) {
   }
 }
 
+/// 以promise形式回传数据到RN端
 RCT_EXPORT_METHOD(passPromiseBackToRN:(NSString *)msg resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
   if (![msg isEqualToString:@""]) {
     resolve(@(YES));
@@ -97,6 +105,7 @@ RCT_EXPORT_METHOD(passPromiseBackToRN:(NSString *)msg resolve:(RCTPromiseResolve
   }
 }
 
+/// 跳转界面，在主线程进行UI操作
 RCT_EXPORT_METHOD(jumpToNativeView) {
   dispatch_async(dispatch_get_main_queue(), ^{
     AppDelegate *delegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
@@ -105,14 +114,17 @@ RCT_EXPORT_METHOD(jumpToNativeView) {
   });
 }
 
+/// 接收通知的方法，接收到通知后发送事件到RN端。RN端接收到事件后可以进行相应的逻辑处理或界面跳转
 - (void)sendCustomEvent:(NSNotification *)notification {
   [self sendEventWithName:kCustomEventName body:@"这是发给RN的字符串"];
 }
 
+/// 重写方法，定义支持的事件集合
 - (NSArray<NSString *> *)supportedEvents {
   return @[kCustomEventName];
 }
 
+/// 重写方法，定义常量
 - (NSDictionary *)constantsToExport {
   return @{@"CustomConstant": @"我是iOS端定义的常量"};
 }
